68424d221a7ce40b794444b59b4f725f82ee868b
KhatlyllenConstant <110945808+KhatlyllenConstant@users.noreply.github.com>
2023-11-24 20:50:23 -0300

    test_app.py

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        # Configurações iniciais para os testes
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_cadastrar_usuario(self):
+        # Adicione testes para cada rota
+        response = self.app.post('/cadastrar_usuario', json={'usuario_id': '1', 'nome': 'Teste'})
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['message'], 'Usuário cadastrado com sucesso')
+
+        # Adicione mais testes conforme necessário
+
+if __name__ == '__main__':
+    unittest.main()
